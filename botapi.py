dc3a9b1226effad16438dca25852accaa65be03b
Phillip Lopo <philliplopo@gmail.com>
2015-06-25 16:14:37 -0700

    No longer require Request object for each api call

--- botapi.py
+++ botapi.py
@@ -242,7 +242,7 @@ class RequestMethod(str, Enum):
 class TelegramBotRPCRequest(metaclass=ABCMeta):
     api_url_base = 'https://api.telegram.org/bot'
 
-    def __init__(self, api_method, token, *, params=None, callback=None, on_error=None, files=None, request_method=RequestMethod.GET):
+    def __init__(self, api_method, token, *, params=None, callback=None, on_result=None, on_error=None, files=None, request_method=RequestMethod.GET):
         self.api_method = api_method
         self.token = token
         self.params = params
@@ -250,6 +250,7 @@ class TelegramBotRPCRequest(metaclass=ABCMeta):
         self.on_error = on_error
         self.files = files
         self.request_method = RequestMethod(request_method)
+        self.on_result = on_result
 
         self.result = None
         self.error = None
@@ -268,10 +269,6 @@ class TelegramBotRPCRequest(metaclass=ABCMeta):
 
         return Request(self.request_method, self._get_url(), data=data, files=files).prepare()
 
-    @abstractmethod
-    def _call_result(self, api_response):
-        raise NotImplemented
-
     def _async_call(self):
         self.error = None
         self.response = None
@@ -282,7 +279,11 @@ class TelegramBotRPCRequest(metaclass=ABCMeta):
         api_response = resp.json()
 
         if api_response.get('ok') == True:
-            self.result = self._call_result(api_response)
+            result = api_response['result']
+            if self.on_result is None:
+                self.result = result
+            else:
+                self.result = self.on_result(result)
 
             if self.callback is not None:
                 self.callback(self.result)
@@ -297,75 +298,49 @@ class TelegramBotRPCRequest(metaclass=ABCMeta):
         self._async_call()
         return self
 
-    def cleanup_params(self, **kwargs):
-        return {name:val for name, val in kwargs.items() if val is not None}
+def _cleanup_params(**kwargs):
+    return {name:val for name, val in kwargs.items() if val is not None}
 
-class getMeRequest(TelegramBotRPCRequest):
-    def __init__(self, token:str, *, callback=None, on_error=None, request_method: RequestMethod=None):
-        super().__init__('getMe', token, callback=callback, on_error=on_error, request_method=request_method)
+def get_me(token, *, callback=None, on_error=None, request_method: RequestMethod=RequestMethod.GET):
+    return TelegramBotRPCRequest('getMe', token, request_method=request_method,
+        on_result=User.from_result, on_error=on_error, callback=callback).run()
 
-    def _call_result(self, api_response):
-        return User.from_result(api_response['result'])
+def send_message(token, chat_id: int, text: str, 
+                 disable_web_page_preview: bool=None, reply_to_message_id: int=None, reply_markup: ReplyMarkup=None, 
+                 *, callback=None, on_error=None, request_method: RequestMethod=RequestMethod.GET):
+    
+    params = _cleanup_params(
+        chat_id=chat_id, 
+        text=text, 
+        disable_web_page_preview=disable_web_page_preview,
+        reply_to_message_id=reply_to_message_id,
+        reply_markup=reply_markup
+        )
 
-class sendMessageRequest(TelegramBotRPCRequest):
-    def __init__(self, token:str, chat_id:int, text:str, 
-                 disable_web_page_preview: bool=None, 
-                 reply_to_message_id: int=None, reply_markup: ReplyMarkup=None, 
-                 *, callback=None, on_error=None, request_method=None):
+    return TelegramBotRPCRequest('sendMessage', token, params=params, request_method=request_method,
+        on_result=Message.from_result, on_error=on_error, callback=callback).run()
 
-        params = self.cleanup_params(chat_id=chat_id, text=text, 
-                                     disable_web_page_preview=disable_web_page_preview, 
-                                     reply_to_message_id=reply_to_message_id, reply_markup=reply_markup)
+def send_photo(token, chat_id: int,  photo: InputFile, 
+               caption: str=None, reply_to_message_id: int=None, reply_markup: ReplyMarkup=None,
+               *, callback=None, on_error=None, request_method: RequestMethod=RequestMethod.POST):
 
-        super().__init__('sendMessage', token, params=params, callback=callback, on_error=on_error, request_method=request_method)
+    files = None
+    if isinstance(photo, InputFile):
+        files = [photo]
+        photo = None
+    elif not isinstance(photo, str):
+        raise Exception('photo must be instance of InputFile or str')
 
-    def _call_result(self, api_response):
-        result = api_response['result']
-        return Message.from_result(result)
+    params = _cleanup_params(
+        chat_id=chat_id,
+        photo=photo,
+        caption=caption,
+        reply_to_message_id=reply_to_message_id,
+        reply_markup=reply_markup
+        )
 
-
-class sendPhotoRequest(TelegramBotRPCRequest):
-    def __init__(self, token, chat_id, photo, caption=None, reply_to_message_id=None, reply_markup=None,
-                 *, callback=None, on_error=None, request_method=None):
-
-        files = None
-        if isinstance(photo, InputFile):
-            files = [photo]
-            photo = None
-        elif not isinstance(photo, str):
-            raise Exception('photo must be instance of InputFile or str')
-
-
-        params = self.cleanup_params(token=token, chat_id=chat_id, photo=photo, caption=caption,
-                                     reply_to_message_id=reply_to_message_id, reply_markup=reply_markup)
-
-        super().__init__('sendPhoto', token, params=params, callback=callback, on_error=on_error,
-                         files=files, request_method=request_method)
-
-    def _call_result(self, api_response):
-        return Message.from_result(api_response['result'])
-
-class TelegramBotRPC:
-    @staticmethod
-    def get_me(token, *, callback=None, on_error=None, request_method: RequestMethod=RequestMethod.GET):
-        return getMeRequest(token, callback=callback, on_error=on_error, request_method=request_method).run()
-
-    @staticmethod
-    def send_message(token, chat_id: int, text: str, disable_web_page_preview: bool=None, 
-                     reply_to_message_id: int=None, reply_markup: ReplyMarkup=None, 
-                     *, callback=None, on_error=None, request_method: RequestMethod=RequestMethod.GET):
-
-        return sendMessageRequest(token, chat_id, text, disable_web_page_preview, reply_to_message_id, reply_markup,
-                                  callback=callback, on_error=on_error, request_method=request_method).run()
-
-    @staticmethod
-    def send_photo(token, chat_id: int,  photo: InputFile, caption: str=None, 
-                   reply_to_message_id: int=None, reply_markup: ReplyMarkup=None,
-                   *, callback=None, on_error=None, request_method: RequestMethod=RequestMethod.POST):
-
-        return sendPhotoRequest(token, chat_id, photo, caption=caption, 
-                                reply_to_message_id=reply_to_message_id, reply_markup=reply_markup,
-                                callback=callback, on_error=on_error, request_method=request_method).run()
+    return TelegramBotRPCRequest('sendPhoto', token, params=params, files=files, request_method=request_method,
+        on_result=Message.from_result, on_error=on_error, callback=callback).run()
 
 
 
@@ -384,8 +359,8 @@ if __name__ == '__main__':
 
     photo = InputFile('photo', InputFileInfo('test.jpg', open('test.jpg', 'rb'), 'image/jpeg'))
 
-    TelegramBotRPC.get_me(test_token, callback=print_result)
-    TelegramBotRPC.send_message(test_token, test_chat_id, 'testing', callback=print_result)
-    TelegramBotRPC.send_photo(test_token, test_chat_id, photo, callback=print_result)
-    TelegramBotRPC.send_photo(test_token, test_chat_id, 'AgADAwADqacxGwpPWQaFLwABSzSkg2Bq-usqAASiGyniRUnk5BdEAAIC',
-                              callback=print_result, on_error=print_error)
+    get_me(test_token, callback=print_result)
+    send_message(test_token, test_chat_id, 'testing', callback=print_result)
+    send_photo(test_token, test_chat_id, photo, callback=print_result)
+    #TelegramBotRPC.send_photo(test_token, test_chat_id, 'AgADAwADqacxGwpPWQaFLwABSzSkg2Bq-usqAASiGyniRUnk5BdEAAIC',
+    #                         callback=print_result, on_error=print_error)
