2afdcad7568ec73017bd04e66fd84d0119dd6c43
Phillip Lopo <philliplopo@gmail.com>
2015-06-26 15:18:13 -0700

    Using rev-parse to specify version for testing

--- setup.py
+++ setup.py
@@ -1,14 +1,27 @@
 from distutils.core import setup
 
+from subprocess import Popen, PIPE
+
+version = None
+with Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=PIPE) as f:
+    version = '1.0.{}'.format(f.stdout.read().decode().strip())
+
+if version is None:
+  raise Exception('Unable to determine version information')
+
 setup(
-  name = 'twx',
-  packages = ['twx'], # this must be the same as the name above
-  version = '1.0',
-  description = 'Unofficial Telegram Bot API Client',
-  author = 'Vince Castellano, Phillip Lopo',
-  author_email = 'surye80@gmail.com, philliplopo@gmail.com',
-  url = 'https://github.com/datamachine/twx', # use the URL to the github repo
-  download_url = 'https://github.com/datamachine/twx/archive/master.tar.gz', # I'll explain this in a second
-  keywords = ['datamachine', 'telex', 'telegram', 'bot', 'api'], # arbitrary keywords
-  classifiers = []
+    name = 'twx',
+    packages = ['twx'], # this must be the same as the name above
+    version = version,
+    description = 'Unofficial Telegram Bot API Client',
+    author = 'Vince Castellano, Phillip Lopo',
+    author_email = 'surye80@gmail.com, philliplopo@gmail.com',
+    url = 'https://github.com/datamachine/twx', # use the URL to the github repo
+    download_url = 'https://github.com/datamachine/twx/archive/master.tar.gz', # I'll explain this in a second
+    keywords = ['datamachine', 'telex', 'telegram', 'bot', 'api', 'rpc'], # arbitrary keywords
+    classifiers = [
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'Programming Language :: Python :: 3.4',
+  ]
 )
