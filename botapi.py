e1f81b27fb7cef6bfe03272b8c5f2a673c68dfbd
Phillip Lopo <philliplopo@gmail.com>
2015-06-25 23:02:16 -0700

    Added docstring to get_updates

--- botapi.py
+++ botapi.py
@@ -587,9 +587,35 @@ def get_user_profile_photos(user_id: int, offset: int=None, limit: int=None, *, 
 
     return TelegramBotRPCRequest('getUserProfilePhotos', params=params, on_result=_process_get_user_profile_photos_result, **request_args).run()
 
-def get_updates(request_args, **kwargs):
+def get_updates(offset: int=None, limit: int=None, timeout: int=None, *, request_args, **kwargs):
     """
-    :param request_args, **kwargs: Args passed down to the TelegramBotRPCRequest
+    Use this method to receive incoming updates using long polling. 
+    Notes
+    1. This method will not work if an outgoing webhook is set up.
+    2. In order to avoid getting duplicate updates, recalculate offset after each server response.
+
+    :param offset: Identifier of the first update to be returned. Must be 
+                   greater by one than the highest among the identifiers of
+                   previously received updates. By default, updates starting
+                   with the earliest unconfirmed update are returned. An update
+                   is considered confirmed as soon as getUpdates is called with
+                   an offset higher than its update_id.
+    :param limit: Limits the number of updates to be retrieved. Values between
+                  1—100 are accepted. Defaults to 100
+    :param timeout: Timeout in seconds for long polling. Defaults to 0, i.e.
+                    usual short polling
+
+    :param request_args: Args passed down to the TelegramBotRPCRequest
+    :param kwargs: Args passed down to the TelegramBotRPCRequest (Overrides request_args)
+
+    :type offset: 
+    :type limit: 
+    :type timeout: 
+    :type request_args:
+    :type kwargs:
+
+    :returns: An Array of Update objects is returned.
+    :rtype: TelegramBotRPCRequest
     """
     #TODO: implement
     raise NotImplemented
