8cd1a7c952ac3329d7f6381da5db13990cea7a4c
Phillip Lopo <philliplopo@gmail.com>
2015-06-26 14:42:00 -0700

    Adding pypi setup

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+  name = 'twx',
+  packages = ['twx'], # this must be the same as the name above
+  version = '1.0',
+  description = 'Unofficial Telegram Bot API Client',
+  author = 'Vince Castellano, Phillip Lopo',
+  author_email = 'surye80@gmail.com, philliplopo@gmail.com',
+  url = 'https://github.com/datamachine/twx', # use the URL to the github repo
+  download_url = 'https://github.com/datamachine/twx/tarball/1.0', # I'll explain this in a second
+  keywords = ['datamachine', 'telex', 'telegram', 'bot', 'api'], # arbitrary keywords
+  classifiers = []
+)
