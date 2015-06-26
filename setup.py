be2e66c8f936027b6187c51789fa71afedff4e91
Phillip Lopo <philliplopo@gmail.com>
2015-06-26 15:23:20 -0700

    download_url matches version now

--- setup.py
+++ setup.py
@@ -2,23 +2,34 @@ from distutils.core import setup
 
 from subprocess import Popen, PIPE
 
-version = None
-with Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=PIPE) as f:
-    version = '1.0.{}'.format(f.stdout.read().decode().strip())
+short_rev = None
+long_rev = None
 
-if version is None:
-  raise Exception('Unable to determine version information')
+with Popen(['git', 'rev-parse', '--short', 'HEAD'], stdout=PIPE) as f:
+    short_rev = f.stdout.read().decode().strip()
+
+with Popen(['git', 'rev-parse', 'HEAD'], stdout=PIPE) as f:
+    long_rev = f.stdout.read().decode().strip()
+
+if short_rev is None:
+  raise Exception('Unable to determine short revision')
+
+if long_rev is None:
+  raise Exception('Unable to determine long revision')
+
+version = '0.9.{short_rev}'.format(short_rev=short_rev)
+download_url = 'https://github.com/datamachine/twx/archive/{long_rev}.tar.gz'.format(long_rev=long_rev)
 
 setup(
     name = 'twx',
-    packages = ['twx'], # this must be the same as the name above
+    packages = ['twx'],
     version = version,
     description = 'Unofficial Telegram Bot API Client',
     author = 'Vince Castellano, Phillip Lopo',
     author_email = 'surye80@gmail.com, philliplopo@gmail.com',
-    url = 'https://github.com/datamachine/twx', # use the URL to the github repo
-    download_url = 'https://github.com/datamachine/twx/archive/master.tar.gz', # I'll explain this in a second
-    keywords = ['datamachine', 'telex', 'telegram', 'bot', 'api', 'rpc'], # arbitrary keywords
+    url = 'https://github.com/datamachine/twx', 
+    download_url = download_url, 
+    keywords = ['datamachine', 'telex', 'telegram', 'bot', 'api', 'rpc'],
     classifiers = [
       'Development Status :: 4 - Beta',
       'Intended Audience :: Developers',
