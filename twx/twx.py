c96216e25ec19f65385ea83750c38b912860d0c4
Vincent Castellano <surye80@gmail.com>
2015-06-27 04:17:45 -0700

    Begin implementation of highlevel twx api.

--- twx/twx.py
+++ twx/twx.py
@@ -1,27 +1,61 @@
-from .mtproto import mtproto
-from .mtproto import rpc
+from enum import Enum
+import botapi
 
+import mimetypes
 from datetime import datetime
-import random
 import hashlib
+from abc import ABCMeta, abstractmethod
 
-class TWX:
-    def __init__(self, api_key, app_id, rsa_cert):
-        self._mtproto = mtproto.MTProto(api_key, app_id, rsa_cert)
-        self._peer_cache = []
-        self._contact_list = []
-        self._dialog_list = []
-        self.id = None
 
-        # Callbacks
-        self.on_binlog_replay_end = None
-        self.on_get_difference_end = None
-        self.on_msg_receive = None
-        self.on_user_update = None
-        self.on_chat_update = None
-        self.on_secret_chat_update = None
+class TWX(metaclass=ABCMeta):
+    def __init__(self):
+        self._on_msg_receive = None
+        self._on_user_update = None
+        self._on_chat_update = None
+        self._on_secret_chat_update = None
 
-    def send_message(self, peer, text, reply=None, link_preview=None, callback=None):
+    @property
+    @abstractmethod
+    def bot_id(self):
+        """
+        :returns: The telegram ID of the current bot.
+        """
+        pass
+
+    @property
+    def on_msg_receive(self):
+        return self._on_msg_receive
+
+    @on_msg_receive.setter
+    def on_msg_receive(self, callback):
+        self._on_msg_receive = callback
+
+    @property
+    def on_user_update(self):
+        return self._on_user_update
+
+    @on_user_update.setter
+    def on_user_update(self, callback):
+        self._on_user_update = callback
+
+    @property
+    def on_chat_update(self):
+        return self._on_chat_update
+
+    @on_chat_update.setter
+    def on_chat_update(self, callback):
+        self._on_chat_update = callback
+
+    @property
+    def on_secret_chat_update(self):
+        return self._on_secret_chat_update
+
+    @on_secret_chat_update.setter
+    def on_secret_chat_update(self, callback):
+        self._on_secret_chat_update = callback
+
+    @abstractmethod
+    def send_message(self, peer: Peer, text: str, reply: int=None, link_preview: bool=None, callback: callable=None):
         """
         Send message to peer.
         :param peer: Peer to send message to.
@@ -30,25 +64,19 @@ class TWX:
         :param link_preview: Whether or not to show the link preview for this message
         :param callback: Callback to call when call is complete.
         """
+        pass
 
-        # messages.sendMessage#fc55e6b5 flags:# peer:InputPeer reply_to_msg_id:flags.0?int message:string random_id:long
-        # reply_markup:flags.2?ReplyMarkup = messages.SentMessage;
-        self._mtproto.rpc_call(rpc.messages.sendMessage(message=text, peer=peer,
-                                                        reply_to_msg_id=reply, reply_markup=link_preview,
-                                                        random_id=int(random.random())),
-                               callback)
-
-    def get_contact_list(self, callback=None):
+    @abstractmethod
+    def get_contact_list(self, callback: callable=None):
         """
         Retrieve contact list.
-        :param callback: Callback to call when server returns of contacts. If not specified, just load the local version
+        :param callback: Callback to call when server returns of contacts. If not specified,
+                         just load the local version
         """
-        if callback is None:
-            return self.contact_list
-        else:
-            self._mtproto.rpc_call(rpc.contacts.getContacts(hash=self._get_contact_list_hash()), callback=callback)
+        pass
 
-    def add_contact(self, phone, first_name, last_name=None, callback=None):
+    @abstractmethod
+    def add_contact(self, phone_number: str, first_name: str, last_name: str=None, callback: callable=None):
         """
         Add contact by phone number and name (last_name is optional).
         :param phone: Valid phone number for contact.
@@ -56,20 +84,21 @@ class TWX:
         :param last_name: Last name to use. Optional.
         :param callback: Callback to call when adding, will contain success status and the current contact list.
         """
-        self._mtproto.rpc_call(rpc.contacts.importContacts(contacts=[rpc.InputContact(client_id=self.id, phone=phone,
-                                                                                      first_name=first_name,
-                                                                                      last_name=last_name)]),
-                               callback=callback)
+        pass
 
-    def del_contact(self, peer, callback=None):
+    @abstractmethod
+    def del_contact(self, peer: Peer, callback: callable=None):
         """
         Delete peer from contact list
         :param peer: Peer to delete
         :param callback: Callback to call when deleting, will contain success status and the current contact list.
         """
-        self._mtproto.rpc_call(rpc.contacts.deleteContact(id=rpc.InputUser(user_id=peer.id)), callback)
+        pass
 
-    def message_search(self, text, callback, peer=None, min_date=None, max_date=None, max_id=None, offset=0, limit=255):
+    @abstractmethod
+    def message_search(self, text: str, callback: callable, peer: Peer=None,
+                       min_date: datetime=None, max_date: datetime=None,
+                       max_id: int=None, offset: int=0, limit: int=255):
         """
         Search for messages.
         :param text: Text to search for in messages
@@ -82,61 +111,183 @@ class TWX:
         :param limit: How many messages to return.
         :return:
         """
-        if max_date:
-            max_date = int((max_date - datetime(1970, 1, 1)).total_seconds())
-
-        if min_date:
-            min_date = int((min_date - datetime(1970, 1, 1)).total_seconds())
-
-        if peer:
-            if peer.type == Group:
-                peer = rpc.inputPeerChat(chat_id=peer.id)
-            elif peer.type == User:
-                peer = rpc.inputPeerContact(user_id=peer.id)
-            else:
-                raise TWXException("Searching a non-supported peer type")
-
-        self._mtproto.rpc_call(rpc.messages.search(peer=peer or rpc.inputPeerEmpty,
-                                                   q=text,
-                                                   filter=rpc.inputMessagesFilterEmpty,
-                                                   min_date=min_date or -1,
-                                                   max_date=max_date or -1,
-                                                   max_id=max_id or -1,
-                                                   offset=offset,
-                                                   limit=limit), callback)
-
-    def set_profile_photo(self, file_path, callback=None):
         pass
+
+    @abstractmethod
+    def set_profile_photo(self, file_path: str, callback: callable=None):
+        """
+        Sets the profile photo for the bot.
+        :param file_path: Path to image file
+        :param callback: Callback to call with the status
+        """
+        pass
+
+    @abstractmethod
+    def create_secret_chat(self, user: User, callback: callable):
+        """
+        Create a secret chat with the user.
+        :param user: User to start secret chat with.
+        :param callback: Will return the chat and meta information.
+        """
+        pass
+
+    @abstractmethod
+    def create_group_chat(self, user_list: list, name: str, callback: callable=None):
+        """
+        Create a new group with the specified list of users, must be at least 2 users.
+        :param user_list: List of Peers.
+        :param name: Name of group.
+        :param callback:
+        """
+        pass
+
+    @abstractmethod
+    def status_online(self):
+        """
+        Sets bot's status to online.
+        """
+        pass
+
+    @abstractmethod
+    def status_offline(self):
+        """
+        Sets bot's status to offline.
+        """
+        pass
+
+
+class TWXBotApi(TWX):
+    def __init__(self, token):
+        super().__init__()
+        
+        self._bot_user = botapi.User(None, None, None, None)
+
+        self.request_args = dict(
+            token=token,
+            request_method=botapi.RequestMethod.POST
+        )
+
+    @property
+    def bot_id(self):
+        return self._bot_user.id
+
+    def update_bot_info(self):
+        self._bot_user = botapi.get_me(request_args=self.request_args)
+
+    def send_message(self, peer: Peer, text: str, reply: int=None, link_preview: bool=None,
+                     callback: callable=None, reply_markup: botapi.ReplyMarkup=None):
+        """
+        Send message to peer.
+        :param peer: Peer to send message to.
+        :param text: Text to send.
+        :param reply: Message object or message_id to reply to.
+        :param link_preview: Whether or not to show the link preview for this message
+        :param callback: Callback to call when call is complete.
+
+        :type reply: int or Message
+        """
+        if isinstance(reply, Message):
+            reply = reply.id
+
+        botapi.send_message(chat_id=peer.id, text=text, disable_web_page_preview=not link_preview,
+                            reply_to_message_id=reply, callback=callback, reply_markup=reply_markup,
+                            request_args=self.request_args)
+
+    def send_photo(self, peer: Peer, photo: str, caption: str=None, reply: int=None, link_preview: bool=None,
+                     callback: callable=None, reply_markup: botapi.ReplyMarkup=None):
+        """
+        Send photo to peer.
+        :param peer: Peer to send message to.
+        :param photo: File path to photo to send.
+        :param caption: Caption for photo
+        :param reply: Message object or message_id to reply to.
+        :param link_preview: Whether or not to show the link preview for this message
+        :param callback: Callback to call when call is complete.
+
+        :type reply: int or Message
+        """
+        if isinstance(reply, Message):
+            reply = reply.id
+
+        photo = botapi.InputFile('photo', botapi.InputFileInfo(photo, open(photo, 'rb'), self._get_mimetype(photo)))
+
+        botapi.send_photo(chat_id=peer.id, photo=photo, caption=caption, disable_web_page_preview=not link_preview,
+                          reply_to_message_id=reply, callback=callback, reply_markup=reply_markup,
+                          request_args=self.request_args)
+
+    def get_contact_list(self, callback=None):
+        """
+        Unsupported in the Bot API
+        """
+        raise TWXUnsupportedMethod()
+
+    def add_contact(self, phone, first_name, last_name=None, callback=None):
+        """
+        Unsupported in the Bot API
+        """
+        raise TWXUnsupportedMethod()
+
+    def del_contact(self, peer, callback=None):
+        """
+        Unsupported in the Bot API
+        """
+        raise TWXUnsupportedMethod()
+
+    def message_search(self, text, callback, peer=None, min_date=None, max_date=None, max_id=None, offset=0, limit=255):
+        """
+        Unsupported in the Bot API
+        """
+        raise TWXUnsupportedMethod()
+
+    def set_profile_photo(self, file_path: str, callback: callable=None):
+        """
+        Unsupported in the Bot API
+        """
+        raise TWXUnsupportedMethod()
 
     def create_secret_chat(self, user, callback=None):
-        pass
+        """
+        Unsupported in the Bot API
+        """
+        raise TWXUnsupportedMethod()
 
     def create_group_chat(self, user_list, name, callback=None):
-        pass
-
-    def restore_msg(self, message, callback=None):
-        pass
+        """
+        Unsupported in the Bot API
+        """
+        raise TWXUnsupportedMethod()
 
     def status_online(self):
-        pass
+        """
+        Unsupported in the Bot API
+        """
+        raise TWXUnsupportedMethod()
 
     def status_offline(self):
-        pass
+        """
+        Unsupported in the Bot API
+        """
+        raise TWXUnsupportedMethod()
 
-    def _get_contact_list_hash(self):
-        return hashlib.md5(','.join(sorted([contact.id for contact in self.contact_list]))).hexdigest()
+    def _get_mimetype(self, file_path):
+        return mimetypes.guess_type(file_path, strict=False)[0] or 'application/octet-stream'
 
+
+class PeerType(str, Enum):
+    GROUP = 'group'
+    USER = 'user'
+    SECRET_CHAT = 'secret_chat'
 
 class Peer:
-    def __init__(self, twx, peer_type=None):
+    def __init__(self, twx, peer_id: int, peer_type: PeerType):
         """
         :param twx: twx.TWX instance associated with this peer.
         :param peer_type: Peer type as a class
         :return: New generic peer
         """
         self.twx = twx
-        self._id = None
-        self._type = peer_type or Peer
+        self._id = peer_id
+        self._type = peer_type
         self.name = None
 
     def send_message(self, text, reply=None, link_preview=None, callback=None):
@@ -187,8 +338,8 @@ class Peer:
 
 
 class Group(Peer):
-    def __init__(self, twx):
-        super().__init__(twx, Group)
+    def __init__(self, twx: TWX, peer_id: int):
+        super().__init__(twx, peer_id, PeerType.GROUP)
         self.user_list = []
 
     def rename_chat(self, name):
@@ -201,16 +352,16 @@ class Group(Peer):
         pass
 
 class User(Peer):
-    def __init__(self, twx):
-        super().__init__(twx, User)
+    def __init__(self, twx: TWX, peer_id: int):
+        super().__init__(twx, peer_id, PeerType.USER)
         self.user_status = {'online': None, 'when': None}
         self.phone = None
         self.first_name = None
         self.last_name = None
 
 class SecretChat(Peer):
-    def __init__(self, twx):
-        super().__init__(twx, SecretChat)
+    def __init__(self, twx: TWX, peer_id: int):
+        super().__init__(twx, peer_id, PeerType.SECRET_CHAT)
         self.user_id = None
 
 class Message:
@@ -252,6 +403,8 @@ class Message:
     def delete_msg(self):
         pass
 
+class TWXUnsupportedMethod(Exception):
+    pass
 
 class TWXException(Exception):
     pass
