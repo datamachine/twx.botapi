39de0316634ef692ab3b6cb29b0e022a589441f4
Phillip Lopo <philliplopo@gmail.com>
2015-06-27 19:11:23 -0700

    Added doc string to Update

--- twx/botapi/botapi.py
+++ twx/botapi/botapi.py
@@ -328,6 +328,16 @@ class Location(_LocationBase):
 
 _UpdateBase = namedtuple('Update', ['update_id', 'message'])
 class Update(_UpdateBase):
+    """This object represents an incoming update.
+
+    Attributes:
+        update_id   (int)     :The update‘s unique identifier. Update identifiers start from a certain 
+                               positive number and increase sequentially. This ID becomes especially handy 
+                               if you’re using Webhooks, since it allows you to ignore repeated updates or to 
+                               restore the correct update sequence, should they get out of order.
+        message     (Message) :*Optional.* New incoming message of any kind — text, photo, sticker, etc.
+        
+    """
     __slots__ = ()
 
     @staticmethod
