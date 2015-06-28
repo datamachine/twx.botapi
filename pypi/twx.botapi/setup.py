14f45eaebe6f61b65dbbac30155106ae5d760ac8
Phillip Lopo <philliplopo@gmail.com>
2015-06-27 21:42:56 -0700

    Breaking apart twx-botapi and twx setup scripts

--- pypi/twx.botapi/setup.py
+++ pypi/twx.botapi/setup.py
@@ -10,7 +10,7 @@ _MINOR_VERSION         = 0
 _MICRO_VERSION         = None
 _PRE_RELEASE_TYPE      = 'b'   # a | b | rc
 _PRE_RELEASE_VERSION   = 3
-_DEV_RELEASE_VERSION   = 115
+_DEV_RELEASE_VERSION   = 116
 
 version = '{}.{}'.format(_MAJOR_VERSION, _MINOR_VERSION)
 
@@ -37,7 +37,8 @@ setup(
     packages = ['twx', 'twx.botapi'],
     version = version,
     description = 'Unofficial Telegram Bot API Client',
-    long_description = open("README.rst").read(),
+    long_description = open("../README.rst").read(),
+    package_dir = {'':'..'},
     author = 'Vince Castellano, Phillip Lopo',
     author_email = 'surye80@gmail.com, philliplopo@gmail.com',
     keywords = ['datamachine', 'telex', 'telegram', 'bot', 'api', 'rpc'],
@@ -57,28 +58,28 @@ setup(
       ]
 )
 
-setup(
-    name = 'twx-botapi',
-    package_dir = {'':'twx'},
-    packages = ['botapi'],
-    version = version,
-    description = 'Standalone version of the Unofficial Telegram Bot API Client from TWX',
-    long_description = open("README.rst").read(),
-    author = 'Vince Castellano, Phillip Lopo',
-    author_email = 'surye80@gmail.com, philliplopo@gmail.com',
-    keywords = ['datamachine', 'telex', 'telegram', 'bot', 'api', 'rpc'],
-    url = 'https://github.com/datamachine/twx', 
-    download_url = download_url, 
-    install_requires=['requests'],
-    platforms = ['Linux', 'Unix', 'MacOsX', 'Windows'],
-    classifiers = [
-      'Development Status :: 4 - Beta',
-      'Intended Audience :: Developers',
-      'License :: OSI Approved :: MIT License',
-      'Operating System :: OS Independent',
-      'Programming Language :: Python :: 3 :: Only',
-      'Programming Language :: Python :: 3.4',
-      'Topic :: Communications :: Chat',
-      'Topic :: Communications :: File Sharing'
-      ]
-)
+# setup(
+#     name = 'twx-botapi',
+#     package_dir = {'':'twx'},
+#     packages = ['botapi'],
+#     version = version,
+#     description = 'Standalone version of the Unofficial Telegram Bot API Client from TWX',
+#     long_description = open("README.rst").read(),
+#     author = 'Vince Castellano, Phillip Lopo',
+#     author_email = 'surye80@gmail.com, philliplopo@gmail.com',
+#     keywords = ['datamachine', 'telex', 'telegram', 'bot', 'api', 'rpc'],
+#     url = 'https://github.com/datamachine/twx', 
+#     download_url = download_url, 
+#     install_requires=['requests'],
+#     platforms = ['Linux', 'Unix', 'MacOsX', 'Windows'],
+#     classifiers = [
+#       'Development Status :: 4 - Beta',
+#       'Intended Audience :: Developers',
+#       'License :: OSI Approved :: MIT License',
+#       'Operating System :: OS Independent',
+#       'Programming Language :: Python :: 3 :: Only',
+#       'Programming Language :: Python :: 3.4',
+#       'Topic :: Communications :: Chat',
+#       'Topic :: Communications :: File Sharing'
+#       ]
+# )
---
+++ pypi/twx/setup.py
@@ -0,0 +1,85 @@
+from setuptools import setup
+from subprocess import Popen, PIPE
+import sys
+
+revision = None
+
+# must match PEP 440
+_MAJOR_VERSION         = 1
+_MINOR_VERSION         = 0
+_MICRO_VERSION         = None
+_PRE_RELEASE_TYPE      = 'b'   # a | b | rc
+_PRE_RELEASE_VERSION   = 3
+_DEV_RELEASE_VERSION   = 116
+
+version = '{}.{}'.format(_MAJOR_VERSION, _MINOR_VERSION)
+
+if _MICRO_VERSION is not None:
+    version += '.{}'.format(_MICRO_VERSION)
+
+if _PRE_RELEASE_TYPE is not None and _PRE_RELEASE_VERSION is not None:
+    version += '{}{}'.format(_PRE_RELEASE_TYPE, _PRE_RELEASE_VERSION)
+
+if 'pypitest' in sys.argv:
+    version += '.dev{}'.format(_DEV_RELEASE_VERSION)
+
+    revision = '9bfadaa27e2991fc3ef0c7a49b5547c145d70973'
+else:
+    revision = version
+
+download_url = 'https://github.com/datamachine/twx/archive/{}.tar.gz'.format(revision)
+
+print(version)
+print(download_url)
+
+setup(
+    name = 'twx',
+    packages = ['twx', 'twx.botapi'],
+    version = version,
+    description = 'Unofficial Telegram Bot API Client',
+    long_description = open("../README.rst").read(),
+    package_dir = {'':'..'},
+    author = 'Vince Castellano, Phillip Lopo',
+    author_email = 'surye80@gmail.com, philliplopo@gmail.com',
+    keywords = ['datamachine', 'telex', 'telegram', 'bot', 'api', 'rpc'],
+    url = 'https://github.com/datamachine/twx', 
+    download_url = download_url, 
+    install_requires=['requests'],
+    platforms = ['Linux', 'Unix', 'MacOsX', 'Windows'],
+    classifiers = [
+      'Development Status :: 4 - Beta',
+      'Intended Audience :: Developers',
+      'License :: OSI Approved :: MIT License',
+      'Operating System :: OS Independent',
+      'Programming Language :: Python :: 3 :: Only',
+      'Programming Language :: Python :: 3.4',
+      'Topic :: Communications :: Chat',
+      'Topic :: Communications :: File Sharing'
+      ]
+)
+
+# setup(
+#     name = 'twx-botapi',
+#     package_dir = {'':'twx'},
+#     packages = ['botapi'],
+#     version = version,
+#     description = 'Standalone version of the Unofficial Telegram Bot API Client from TWX',
+#     long_description = open("README.rst").read(),
+#     author = 'Vince Castellano, Phillip Lopo',
+#     author_email = 'surye80@gmail.com, philliplopo@gmail.com',
+#     keywords = ['datamachine', 'telex', 'telegram', 'bot', 'api', 'rpc'],
+#     url = 'https://github.com/datamachine/twx', 
+#     download_url = download_url, 
+#     install_requires=['requests'],
+#     platforms = ['Linux', 'Unix', 'MacOsX', 'Windows'],
+#     classifiers = [
+#       'Development Status :: 4 - Beta',
+#       'Intended Audience :: Developers',
+#       'License :: OSI Approved :: MIT License',
+#       'Operating System :: OS Independent',
+#       'Programming Language :: Python :: 3 :: Only',
+#       'Programming Language :: Python :: 3.4',
+#       'Topic :: Communications :: Chat',
+#       'Topic :: Communications :: File Sharing'
+#       ]
+# )
