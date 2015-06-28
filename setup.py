ac52c0b683165515860f2b04e229206436736b5e
Phillip Lopo <philliplopo@gmail.com>
2015-06-27 22:29:15 -0700

    Updating pypi setup

--- setup.py
+++ setup.py
@@ -5,14 +5,15 @@ import sys
 revision = None
 
 # must match PEP 440
-_MAJOR_VERSION         = 1
-_MINOR_VERSION         = 0
+_MAJOR_VERSION         = 0
+_MINOR_VERSION         = 5
 _MICRO_VERSION         = None
-_PRE_RELEASE_TYPE      = 'b'   # a | b | rc
+_PRE_RELEASE_TYPE      = 'a'   # a | b | rc
 _PRE_RELEASE_VERSION   = 3
 _DEV_RELEASE_VERSION   = 116
 
 version = '{}.{}'.format(_MAJOR_VERSION, _MINOR_VERSION)
+revision = None
 
 if _MICRO_VERSION is not None:
     version += '.{}'.format(_MICRO_VERSION)
@@ -20,12 +21,11 @@ if _MICRO_VERSION is not None:
 if _PRE_RELEASE_TYPE is not None and _PRE_RELEASE_VERSION is not None:
     version += '{}{}'.format(_PRE_RELEASE_TYPE, _PRE_RELEASE_VERSION)
 
-if 'pypitest' in sys.argv:
+if _DEV_RELEASE_VERSION is not None:
     version += '.dev{}'.format(_DEV_RELEASE_VERSION)
-
-    revision = '9bfadaa27e2991fc3ef0c7a49b5547c145d70973'
+    revision = 'master'
 else:
-    revision = version
+  revision = version
 
 download_url = 'https://github.com/datamachine/twx/archive/{}.tar.gz'.format(revision)
 
@@ -36,9 +36,8 @@ setup(
     name = 'twx',
     packages = ['twx', 'twx.botapi'],
     version = version,
-    description = 'Unofficial Telegram Bot API Client',
-    long_description = open("../README.rst").read(),
-    package_dir = {'':'..'},
+    description = 'Telegram Bot API and MTProto Client and Abstraction Layer',
+    long_description = open("README.rst").read(),
     author = 'Vince Castellano, Phillip Lopo',
     author_email = 'surye80@gmail.com, philliplopo@gmail.com',
     keywords = ['datamachine', 'telex', 'telegram', 'bot', 'api', 'rpc'],
