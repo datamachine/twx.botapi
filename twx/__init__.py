6c89ff451cfe64908071e93d042606d36966addf
Phillip Lopo <philliplopo@gmail.com>
2015-06-29 15:06:03 -0700

    Expand the twx namespace

--- twx/__init__.py
+++ twx/__init__.py
@@ -1,7 +1,8 @@
 import sys
 
 """
-Only import twx modules on supported pytohn versions. Type hinting breaks testing 2.7 against twx.botapi
+Extend the twx namespace
 """
-if sys.version_info[0] == 3 and sys.version_info[1] >=4:
-	from . twx import *
+if sys.version_info[0] == 3 and sys.version_info[1] >=2:
+	from pkgutil import extend_path
+	__path__ = extend_path(__path__, __name__)
