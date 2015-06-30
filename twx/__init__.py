5ded537b004a35910a4d2c6ce8d080fa2a616538
Phillip Lopo <philliplopo@gmail.com>
2015-06-29 22:42:40 -0700

    pep8ified

--- twx/__init__.py
+++ twx/__init__.py
@@ -3,6 +3,6 @@ import sys
 """
 Extend the twx namespace
 """
-if sys.version_info[0] == 3 and sys.version_info[1] >=2:
-	from pkgutil import extend_path
-	__path__ = extend_path(__path__, __name__)
+if sys.version_info[0] == 3 and sys.version_info[1] >= 2:
+    from pkgutil import extend_path
+    __path__ = extend_path(__path__, __name__)
